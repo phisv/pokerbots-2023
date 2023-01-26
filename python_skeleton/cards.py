84543540f3327792f8a605327fe1167932ae3339
dianashklover <68488944+dianashklover@users.noreply.github.com>
2023-01-25 22:30:58 -0500

    Fixed post-flop analysis

--- python_skeleton/cards.py
+++ python_skeleton/cards.py
@@ -11,15 +11,24 @@ def analyze(cards):
 	cur = cur_val(cards)
 	out = outs(cards)
 	return cur, out
+
+def pot(outs, num_cards):
+	remaining = 2 + int(num_cards == 3)
+	other = 52 - num_cards
+	win_rate = 1 - (((other - outs) / other) ** remaining)
+	return win_rate
+
 def outs(cards):
 	good = 0
 	valid = 0
-	current = cur_val(hand) // 1000000
+	current = cur_val(cards) // 10000000
 	for card in CARDS:
 		if card not in cards:
 			valid += 1
-			temp = cur_val(card + cards) // 1000000
+			temp = cur_val([card] + cards) // 10000000
 			if temp > current:
 				good += 1
 	return good
 
+if __name__ == '__main__':
+	print(analyze(['Ts','7c','2h','Ah','Ac']))
